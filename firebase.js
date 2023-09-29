4e53663c5c557002633eb0d9f98aae02c2305e58
Cedric LEONETTI <cleonetti@myges.fr>
2023-09-29 12:15:22 +0200

    Modification de fichiers

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import { initializeApp } from 'firebase/app'
 
 const app = initializeApp({
-  apiKey: "", // TODO
-  authDomain: "", // TODO
+  apiKey: "AIzaSyC_VT01kjhu-Fw8cwP7N8bhSBbCJP9gBJE",
+  authDomain: "esgi-cloud-cl2.firebaseapp.com",
   databaseURL: "", // TODO
-  projectId: "", // TODO
+  projectId: "esgi-cloud-cl2",
   storageBucket: "" // TODO
 })
 
